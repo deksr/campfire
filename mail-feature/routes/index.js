4facc5e2b11faab2eca53d9b217c37b5f6375b65
devatha k <k.n.devs@gmail.com>
2016-08-05 20:59:22 -0400

    added ajax

--- mail-feature/routes/index.js
+++ mail-feature/routes/index.js
@@ -5,7 +5,7 @@ var nodemailer = require('nodemailer');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('rough', { title: 'Express' });
 });
 
 router.post('/pluto', handleSayHello); 
