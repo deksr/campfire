729f41cbffb6755f239f358b39c8ce501b8ce267
devatha k <k.n.devs@gmail.com>
2016-08-09 18:50:40 -0400

    some random

---
+++ progressive-web/http-server-cache-pwapp/public/sw.js
@@ -0,0 +1,50 @@
+console.log("hello from sw.js")
+
+// The files we want to cache
+var CACHE_NAME = 'devs-site-cache-v1-yoho';
+var urlsToCache = [
+  '/',
+  '/style.css',
+  '/morescripts.js'
+];
+
+// Set the callback for the install step
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+  // event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== CACHE_NAME) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+              return response || fetch(e.request);
+
+      }
+    )
+  );
+});
+
+
