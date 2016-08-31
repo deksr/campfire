ae694d308566522b90fd314b9e57e4b99a976c3e
devatha k <k.n.devs@gmail.com>
2016-08-30 21:36:25 -0400

    url matching

--- progressive-web/news-project/serviceworker.js
+++ progressive-web/news-project/serviceworker.js
@@ -1,6 +1,6 @@
-var dataCacheName = 'breaking-news-v2';
+var dataCacheName = 'breaking-news-cache-v1';
 
-var cacheName = 'breaking-news-v1';
+// var cacheName = 'breaking-news-cachev1';
 var filesToCache = [
   '/',
   '/index.html',
@@ -10,7 +10,6 @@ var filesToCache = [
 
 self.addEventListener('install', function(e) {
   console.log('ServiceWorker is Installed');
-
   e.waitUntil(
     caches.open(dataCacheName).then(function(cache) {
       console.log('Caching the app shell');
@@ -20,34 +19,6 @@ self.addEventListener('install', function(e) {
 });
 
 
-
-self.addEventListener('fetch', function(e) {
-  console.log('ServiceWorker is being Fetched', e.request.url);
-  var url = new URL(e.request.url);
-
-  if (url.origin == location.origin && url.pathname == '/'){
-    e.respondWith(caches.match('/index.html'))
-    console.log("kaskaskashdkjshdkajhsdkahsdkahs")
-    return;
-  }
-
-  // e.respondWith(
-  //   caches.match(e.request).then(function(response) {
-  //     return response || fetch(e.request);
-  //   })
-  // );
-
-  e.respondWith(
-      fetch(e.request)
-        .then(function(response) {
-          return caches.open(dataCacheName).then(function(cache) {
-            cache.put(e.request.url, response.clone());
-            console.log('ServiceWorker Fetched & Cached the Data' + response);
-            return response;
-          });
-        })
-    );
-});
 
 
 
@@ -67,27 +38,30 @@ self.addEventListener('activate', function(e) {
 
 
 
-
 self.addEventListener('fetch', function(e) {
   console.log('ServiceWorker is being Fetched', e.request.url);
-  // debugger;
-
-  var dataUrl = 'secret key';
-  if (e.request.url.indexOf(dataUrl) === 0) {
+    var dataUrl = 'secret-key';
+    if (e.request.url.indexOf(dataUrl) === 0) {
     // Put data handler code here
-  } else {
-   e.respondWith(
-      fetch(e.request)
-        .then(function(response) {
-          return caches.open(dataCacheName).then(function(cache) {
-            cache.put(e.request.url, response.clone());
-            console.log('ServiceWorker Fetched & Cached the Data');
-            return response;
-          });
-        })
-    );
-  }
+    console.log("data-handler code is runing");
+      if("indexedDB" in window) { 
+
+      }
+
+
+    }else {
+      e.respondWith(
+      caches.match(e.request).then(function(response) {
+        return response || fetch(e.request);
+      })
+      );
+    } 
 });
+
+
+
+
+
 
 
 
