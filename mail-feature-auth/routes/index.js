9f85c780ee0289047b2cfc01e50d4c38d408524d
devatha k <k.n.devs@gmail.com>
2016-09-01 20:21:07 -0400

    some del, some add

---
+++ mail-feature-auth/routes/index.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+var nodemailer = require('nodemailer');
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('rough', { title: 'Express' });
+});
+
+router.post('/pluto', handleSayHello); 
+
+function handleSayHello(req, res) {
+	console.log(req.body.from)
+	var transporter = nodemailer.createTransport('smtps://username%40gmail.com:password@smtp.gmail.com');
+
+
+    var mailOptions = {
+    from: req.body.from, // sender address(but this is not working)
+    to: 'username@gmail.com', // list of receivers
+    subject: req.body.from, // Subject line but use from instead
+    text: req.body.text//, // plaintext body
+    // html: '<b>Hello world ✔</b>' // You can choose to send an HTML body instead
+    };
+   
+   transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+        console.log(error);
+        // res.json({yo: 'error'});
+    }else{
+        console.log('Message sent: ' + info.response);
+        // res.json({yo: info.response});
+      };
+     });
+
+   res.redirect('/');
+
+}
+
+module.exports = router;
