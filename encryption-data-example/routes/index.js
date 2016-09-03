ae2fbf37b40c6429918392657c3be461665ccf5f
devatha k <k.n.devs@gmail.com>
2016-09-03 01:17:56 -0400

    encrypted data got it

--- encryption-data-example/routes/index.js
+++ encryption-data-example/routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var CryptoJS = require("crypto-js");
+
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+
+router.post('/pluto', decrypter); 
+
+function decrypter(req, res) {
+	console.log(req.body.messagesend);
+	var gotEncryptedmessage = req.body.messagesend;
+
+	var decrypted = CryptoJS.AES.decrypt(gotEncryptedmessage, "pass phrase").toString(CryptoJS.enc.Utf8);
+
+  console.log(decrypted)
+
+   res.redirect('/');
+
+}
 
 module.exports = router;
