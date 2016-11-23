18aee50118b3d4707696037f2dae8e90d7287636
devatha k <k.n.devs@gmail.com>
2016-11-23 15:35:24 -0500

    added basic coponent

--- react-related/twitter-api-react-book/src/from-book.js
+++ react-related/twitter-api-react-book/src/from-book.js
@@ -2,3 +2,31 @@ import ReactDOM from 'react-dom';
 import React, { Component } from 'react';
 
 
+
+
+// parent of User
+var App = React.createClass({
+
+	getInitialState: function () {
+    return {
+      isHidden: false
+    }; 
+  },
+
+  render: function () {
+
+  	if (this.state.isHidden){
+  		return null;
+  	}
+    return (
+      <div> 
+        <h1> React component </h1>
+      </div>
+    )
+  } 
+});
+
+
+// parent of App
+ReactDOM.render(<App/>, document.getElementById("root"));
+
