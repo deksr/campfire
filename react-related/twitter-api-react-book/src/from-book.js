33b25f4a184d0ddbf880ed35dbf168ce0b9ba65c
devatha k <k.n.devs@gmail.com>
2016-11-23 16:57:47 -0500

    adding state

--- react-related/twitter-api-react-book/src/from-book.js
+++ react-related/twitter-api-react-book/src/from-book.js
@@ -2,26 +2,43 @@ import ReactDOM from 'react-dom';
 import React, { Component } from 'react';
 
 
-
+// initially we set it as true but when we click the event, it should turn to falSE. If it is true then render
+// one type of  element
 
 // parent of User
 var App = React.createClass({
 
 	getInitialState: function () {
     return {
-      isHidden: false
+      isHeaderHidden: true,
+      title: 'Stateful React Component'
     }; 
   },
 
-  render: function () {
+  handleClick: function () {
 
-  	if (this.state.isHidden){
-  		return null;
-  	}
+    this.setState({
+      isHeaderHidden: !this.state.isHeaderHidden
+    });
+    console.log("console from event"  + " "  + this.state.isHeaderHidden)
+  },
+
+  render: function () {
+    console.log("console from render" + " "  + this.state.isHeaderHidden)
+
+  	if (this.state.isHeaderHidden){
+      return (
+      	<div> 
+          <button onClick= {this.handleClick}> press me </button>
+        </div>  	
+      )
+    }
+
     return (
-      <div> 
-        <h1> React component </h1>
-      </div>
+    	<div> 
+          <h1> {this.state.title} </h1>
+          <button onClick= {this.handleClick}> press me </button>
+        </div> 
     )
   } 
 });
