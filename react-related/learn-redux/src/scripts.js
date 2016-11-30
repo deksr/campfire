b724324b38b3d0b060c4b9aba055e89e1d7725df
devatha k <k.n.devs@gmail.com>
2016-11-29 22:41:20 -0500

    notes

--- react-related/learn-redux/src/scripts.js
+++ react-related/learn-redux/src/scripts.js
@@ -1,12 +1,22 @@
 import ReactDOM from 'react-dom';
 import React, { Component } from 'react';
 
+// var stringword = "Devatha"
+// console.log(stringword.substring(stringword.length - 1)) 
+
 var Users = React.createClass({
+
+	   
 
   render: function () {
     return (
       <div>
        hello there
+
+     
+
+
+   
       </div>
     )
   } 
