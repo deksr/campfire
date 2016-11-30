bef7023c3e3fbc2667871d0fc7cd3ad430ba3930
devatha k <k.n.devs@gmail.com>
2016-11-29 23:07:21 -0500

    delete a word function

--- react-related/learn-redux/src/scripts.js
+++ react-related/learn-redux/src/scripts.js
@@ -4,23 +4,43 @@ import React, { Component } from 'react';
 // var stringword = "Devatha"
 // console.log(stringword.substring(stringword.length - 1)) 
 
-var Users = React.createClass({
+var App = React.createClass({
+	getInitialState: function(){
+    return {
+      theWord: ''
+    } 
+  },
 
-	   
+
+
+  displayFunction: function(e){
+  	console.log(e.target.value)
+  	this.setState({
+    	theWord: e.target.value
+    });
+  }, 
+
+
+  deleteLetter: function(){
+  	console.log(this.state.theWord)
+  	var typedWord = this.state.theWord;
+  	this.setState({
+    	theWord: typedWord.substring(0, typedWord.length - 1)
+    });
+  },
+  
 
   render: function () {
     return (
       <div>
-       hello there
-
-     
-
-
-   
+        <p> Delete letter app</p>
+        <input type="text" onChange={this.displayFunction}/>
+        <p> {this.state.theWord}</p> 
+        <button onClick={this.deleteLetter}> Delete Last letter</button>
       </div>
     )
   } 
 });
 
 
-ReactDOM.render(<Users/>, document.getElementById("root"));
+ReactDOM.render(<App/>, document.getElementById("root"));
