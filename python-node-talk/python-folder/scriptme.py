c36a919aaedbee79ea6566920b5e06874efe3a97
devatha k <k.n.devs@gmail.com>
2016-12-04 20:24:49 -0500

    added event registry

--- python-node-talk/python-folder/scriptme.py
+++ python-node-talk/python-folder/scriptme.py
@@ -1,9 +1,11 @@
 print("Hello, World!")
 
 from flask import Flask, render_template, request
-
+from eventregistry import *
 
 import requests
+import time
+
 
 
 app = Flask(__name__)
@@ -13,11 +15,21 @@ def index():
   print ("Hello World!")
 
 
+# below code works
+# er = EventRegistry("http://eventregistry.org", verboseOutput = True)
+# q = QueryEvents()
+# q.addConcept(er.getConceptUri("Star Wars"))   
+# q.addRequestedResult(RequestEventsInfo(sortBy = "date", count=10))   # return event details for last 10 events
+# print er.execQuery(q)
+
+
+
+
   # below commented out code is working. this is using the request module
-  response = requests.get("https://newsapi.org/v1/articles?source=techcrunch&apiKey=60941c39a76e4f14902097a5030f4cab")
-  # jsonify(result)
-  json_object=response.text
-  return json_object
+  # response = requests.get("https://newsapi.org/v1/articles?source=techcrunch&apiKey=60941c39a76e4f14902097a5030f4cab")
+  # # jsonify(result)
+  # json_object=response.text
+  # return json_object
 
 if __name__ == "__main__":
  app.run()
