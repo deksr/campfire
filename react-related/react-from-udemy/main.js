9a19f9765282b055ae6a1019573fe6a7d7c8bda5
devatha k <k.n.devs@gmail.com>
2016-12-09 18:06:04 -0500

    added package.json dependencies

--- react-related/react-from-udemy/main.js
+++ react-related/react-from-udemy/main.js
@@ -1,4 +1,5 @@
 // import Es6learn from './00-es6/es6-learn.js';
-import Chapter01 from './01-chapter1/component1.js';
+// import Chapter01 from './01-chapter1/component1.js';
+import Chapter01 from './02-chapter2/component2.js';
 
 
