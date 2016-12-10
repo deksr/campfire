e765110da174f9129396af4a82417a81312e210b
devatha k <k.n.devs@gmail.com>
2016-12-10 01:05:53 -0500

    syntatic sugar and api data working

--- react-related/react-from-udemy/02-chapter2/component2.js
+++ react-related/react-from-udemy/02-chapter2/component2.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import ApiKey from './api-key-env.js'
-import Searchbar from './searchBar.js'
+import Searchbar from './searchBar.js';
+import Ytsearch from 'youtube-api-search';
+
+
+
+var API_SECRET= 'n,n,n,n,mn,';
+
 
  
 class App extends React.Component {
+
+	constructor(props){
+		super(props);
+		this.state = {videos: []}
+
+		Ytsearch({key: API_SECRET, term: 'surfboards'}, (videos) => {
+      console.log(videos);
+      // this.setState({videos: videos});
+      this.setState({videos});//syntatic sugar
+
+    })	
+	}
+
 
   render() {
     return (
