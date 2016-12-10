db207208760c28ee3381ab50fecca1f32f0acfa6
devatha k <k.n.devs@gmail.com>
2016-12-09 22:31:17 -0500

    added states and events

--- react-related/react-from-udemy/02-chapter2/component2.js
+++ react-related/react-from-udemy/02-chapter2/component2.js
@@ -9,7 +9,7 @@ class App extends React.Component {
   render() {
     return (
     <div>
-       <Searchbar />
+       <Searchbar/>
       </div>
     )
     
--- react-related/react-from-udemy/02-chapter2/searchBar.js
+++ react-related/react-from-udemy/02-chapter2/searchBar.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react'; //this replaces react.component { }
 import ReactDOM from 'react-dom';
 
-class Searchbar extends React.Component {
+
+
+
+class Searchbar extends Component {
+
+	// initialize the state inside the class based component
+	constructor(props){
+		super(props);//
+		this.state = {term: 'Enter search term'}
+	}
+	
+	// onInputChange(e) {
+	// 	console.log(e.target.value)
+	// }
 
   render() {
     return (
-    <div>
- Search <input/>    
-</div>
+	    <div>
+	      { /* Search <input onChange={this.onInputChange}/> */ }
+	      { /*  Search <input onChange={(e)=>{console.log(e.target.value)}}/> //can also be written like this */ } 
+	      <input 
+	        value= {this.state.term} 
+	        onChange={(e)=>{this.setState({term: e.target.value})}}/> 
+   
+	    </div>
      )
   }
 }
