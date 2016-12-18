b8f91a67c62d6ef3efd34713844948b4c466687c
devatha k <k.n.devs@gmail.com>
2016-12-17 19:49:13 -0500

    chrome storage

--- react-chrome-extension/hello.js
+++ react-chrome-extension/hello.js
@@ -3,11 +3,35 @@ import ReactDOM from 'react-dom';
  
 var MainApp = React.createClass({
 
+
+  // learning how to save data in chrome extension's storage
+	submitForm: function (e){
+	  event.preventDefault(); 
+		console.log("clicked");
+		console.log(this.refs.pinky.value);
+
+		var oldWord = [this.refs.pinky.value, "hellothere"];
+		var neWord = [];
+
+		chrome.storage.sync.set({'oldvalue': oldWord}, function() {
+          // Notify that we saved.
+          console.log('Settings saved');
+        });
+
+		chrome.storage.sync.get('oldvalue', function(result){
+			 console.log(result);
+		})
+
+	},
+	//************
+
   render: function () {
     return (
       <div>
-          hello there from react
-          how cool right? yes
+        <p>hello there from react </p>
+        <input type="text" ref="pinky"/> 
+        <button onClick={this.submitForm}> click me! </button>
+
       </div>
     )
   }
